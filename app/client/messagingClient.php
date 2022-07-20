0d1d68a19668aa40bb8a8322bd5c0e746fa29672
Gideon Akomeng <gideonakomeng@Gideons-MacBook-Pro.local>
2022-07-19 20:01:10 -0400

    added logging to messenger

--- app/client/messagingClient.php
+++ app/client/messagingClient.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../api_provider/ApiProvider.php';
-
+require_once __DIR__ . '/../common/logger.php';
 interface MessagingClient
 {
     public function send($sender, $message, $destination, $msgId, $token);
@@ -8,13 +8,20 @@ interface MessagingClient
 
 class VodafoneMessagingCleint implements MessagingClient
 {
+    private Logger $logger;
+    public function __construct()
+    {
+        $this->logger = Logger::getDefaultInstanceWithId($this);
+    }
 
     public function send($sender, $message, $destination, $msgId, $token)
     {
         $url = "https://sdp.vodafone.com.gh/vfgh/gw/messaging/v1/outbound";
         $body = json_encode(array("address" => [$destination], "senderAddress" => $sender, "outboundSMSTextMessage" => array("message" => $message), "clientCorrelator" => "$msgId", "receiptRequest" => array()), true);
         $headers = ["content-type:application/json", "Authorization:Bearer $token"];
+        $this->logger->debug("Sending message: $msgId->" . $body);
         $resp = ApiProvider::send(new ApiRequest($url, $headers, $body), HttpMethod::POST);
+        $this->logger->debug("Received response: $msgId->" . $body);
         return $resp;
     }
 }
